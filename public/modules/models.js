4e7da25bbe4f1c7f054d448dbf7641ba4bbe6267
AntonyMo <moantonymo@gmail.com>
2019-03-11 14:51:04 +0300

    models doc added

--- public/modules/models.js
+++ public/modules/models.js
@@ -2,7 +2,15 @@
 
 (function() {
   class UserModel {
-    constructor(username, email, login, score, img = null) {
+    /**
+     * Default UserModel constructor
+     * @param {string} username - user name
+     * @param {string} email - user email
+     * @param {string} login - user login
+     * @param {string} score - user score
+     * @param {string} img - user avatar path
+     */
+    constructor(username, email, login, score, img = '') {
       this._username = username;
       this._email = email;
       this._login = login;
@@ -10,22 +18,42 @@
       this._score = score;
     }
 
+    /**
+     * Returns user name
+     * @return {string} - user name
+     */
     getUsername() {
       return this._username;
     }
 
+    /**
+     * Returns user email
+     * @return {string} - user email
+     */
     getEmail() {
       return this._email;
     }
 
+    /**
+     * Returns user avatar path
+     * @return {string} - avatar path
+     */
     getImg() {
       return this._img;
     }
 
+    /**
+     * Returns user score
+     * @return {string} - user score
+     */
     getScore() {
       return this._score;
     }
 
+    /**
+     * Updates username
+     * @param {string} username - new user name
+     */
     updateUsername(username) {
       this._username = username;
     }
