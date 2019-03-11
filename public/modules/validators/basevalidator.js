4d2762088cc440dcb28d3b94b86bc053490dd1df
AntonyMo <moantonymo@gmail.com>
2019-03-11 23:09:16 +0300

    regexp is now escaped by linter

--- public/modules/validators/basevalidator.js
+++ public/modules/validators/basevalidator.js
@@ -32,6 +32,7 @@
      * @private
      */
     _correctEmail(email) {
+      // eslint-disable-next-line
       const emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return Boolean(email.match(emailRegexp));
     }
