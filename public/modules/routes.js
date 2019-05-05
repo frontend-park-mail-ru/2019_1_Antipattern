8dae90475227d78f5b8ae58990d5509eb5af3b27
indiagolph99 <savva.gorodetsky@gmail.com>
2019-05-05 17:41:16 +0300

    meh

--- public/modules/routes.js
+++ public/modules/routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import {showErrorMsg, clearErrors} from './utils.js';
-import {Router} from './router.js';
+import { showErrorMsg, clearErrors } from './utils.js';
+import { Router } from './router.js';
 // import {sendMsg} from './chatws.js'
 /**
  * Base view class
@@ -109,6 +109,46 @@ class IndexRoute extends BaseRoute {
     this._rootEl.innerHTML = Handlebars.templates['menu.html']({
       isAuthorized: value,
     });
+
+    let modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    let btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    // let span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function () {
+      modal.style.display = "block";
+      document.getElementById("defaultOpen").click();
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() {
+    //   modal.style.display = "none";
+    // }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    window.openTab = function (evt, cityName) {
+      console.log('heh');
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(cityName).style.display = "block";
+      evt.currentTarget.className += " active";
+    }
   }
 
   /**
