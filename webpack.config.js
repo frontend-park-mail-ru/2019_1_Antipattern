0a1b0bdff91d1df4e525de3fb7a03a391021a5d7
Savva <savva.gorodetsky@gmail.com>
2019-05-20 19:45:54 +0300

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,23 @@ const path = require('path');
 
 module.exports = {
   entry: './public/js/index.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
