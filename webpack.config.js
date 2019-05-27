007ac2049152c25e84b09f189daea5e0ec111fbd
Savva <savva.gorodetsky@gmail.com>
2019-05-27 18:25:42 +0300

    sin bin

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  }
 };
