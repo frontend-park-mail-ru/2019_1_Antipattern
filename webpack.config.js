8cdf9ea05c691224896a14d707ad684312572b81
Savva <savva.gorodetsky@gmail.com>
2019-05-27 18:12:30 +0300

    linter fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './public/js/index.js',
-  mode: 'production',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/dist'),
